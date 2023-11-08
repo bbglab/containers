57258662d6634cf653439a777db12d8abae7e74c
FerriolCalvet <ferriolcalvet@gmail.com>
2023-11-08 12:49:07 +0100

    adding Dockerfiles from my containers

---
+++ bgtools/test.py
@@ -0,0 +1,2 @@
+from bgreference import hg38
+print(hg38(1, 1300000, 3000))
---
+++ sigprofiler_matrix/download_genomes.py
@@ -0,0 +1,7 @@
+from SigProfilerMatrixGenerator import install as genInstall
+
+genInstall.install('GRCh38')
+genInstall.install('GRCh37')
+genInstall.install('mm10')
+genInstall.install('mm9')
+
